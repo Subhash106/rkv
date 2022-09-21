9737139d798af0ab3a266a983a99b2f90c7ae32b
SUBASH CHANDRA (CRGL-THIRDPARTY.COM) <subash_chandra@crgl-thirdparty.com>
2022-09-21 07:43:47 +0530

    Initail commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+const App = () => {
+  return <div className="app">Welcome to the react! update</div>;
+};
+
+export default hot(module)(App);
