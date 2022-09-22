901de05e2f23b0d006ca3c9dad77bda964fb83af
SUBASH CHANDRA (CRGL-THIRDPARTY.COM) <subash_chandra@crgl-thirdparty.com>
2022-09-22 18:36:54 +0530

    Modified Modal component

--- src/components/shared/Modal/index.js
+++ src/components/shared/Modal/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Backdrop from '../Backdrop';
+import Button from '../Button';
 import './style.css';
 
 const Modal = props => {
-  const modal = <div className="modal">{props.children}</div>;
+  const { onClose, heading, onSubmit, className } = props;
+  const modalClassName = `modal ${className}`;
+  const modal = (
+    <>
+      <Backdrop />
+
+      <div className={modalClassName}>
+        <header className="modal-header">
+          <h1>{heading}</h1>
+          <Button className="btn btn-small" onClick={onClose}>
+            X
+          </Button>
+        </header>
+        {props.children}
+        <footer className="modal-footer">
+          <Button onClick={onClose}>Close</Button>
+          <Button onClick={onSubmit}>Submit</Button>
+        </footer>
+      </div>
+    </>
+  );
   return ReactDOM.createPortal(modal, document.getElementById('modal-root'));
 };
 
