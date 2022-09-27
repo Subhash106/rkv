89377f964021320609f537a30098db3d6a48a476
SUBASH CHANDRA (CRGL-THIRDPARTY.COM) <subash_chandra@crgl-thirdparty.com>
2022-09-27 22:06:21 +0530

    Added caching

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{png,jpg,html,json,js}"
-  ],
-  "swDest": "public/service-worker.js",
-  "globIgnores": [
-    "../workbox-cli-config.js"
-  ]
+  globDirectory: 'public/',
+  globPatterns: ['**/*.{png,jpg,html,json,js}'],
+  swSrc: 'public/sw-base.js',
+  swDest: 'public/service-worker.js',
+  globIgnores: ['../workbox-cli-config.js']
 };
