6f4b11d30e20cea907767500852aea3b19f892c5
Subash_Chandra <s.chandra106@gmail.com>
2023-08-17 07:32:07 +0530

    upgraded webpack to version 5

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,11 +6,8 @@ const { merge } = require('webpack-merge');
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
-    port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
-    hotOnly: true,
-    historyApiFallback: true
+    static: 'dist',
+    port: 3000
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
 });
